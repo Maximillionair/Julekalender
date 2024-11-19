5c0c052ee9aa3b04d85489fd82ce0bb3eab4df83
Maximillionair <maxmillian.heim@outlook.com>
2024-11-19 14:52:35 +0100

    express og gitignore
    forbedret innholdet til .gitignore og innstallerte express. tror jeg

--- routes/defaultroutes.js
+++ routes/defaultroutes.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = require('express').Router();
 
-router.get('/', index);
+// router.get('/', index);
+
+router.get('/', (req, res)=>{
+    res.render('index');
+})
 
 module.exports = router;
