cc7cfbf2cffd1bb00ffd73f6f70fe1f7b71738bb
Maximillionair <maxmillian.heim@outlook.com>
2024-11-19 15:02:54 +0100

    prøver fremdeles connecte DB

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require("express")
+const path = require("path")
 const app = express()
 const routes = require('./routes/defaultroutes');
 
-app.set("view engine", "ejs");
-app.use(express.static(__dirname + '/public/'));
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", 'ejs');
+// Middleware to handle form data and JSON payloads
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // To handle URL-encoded data
 app.use(routes);
+
+
+// Start the server on port 3000
+const PORT = process.env.PORT || 3100;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`)
+});
