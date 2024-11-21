15b807c845911cf78ecb9f570a3f08ccdcfe7668
Maximillionair <maxmillian.heim@outlook.com>
2024-11-21 09:00:59 +0100

    linking til css
    link taggen som refererte til css filen var ikke skrevet riktig og definerte ikke cssen som en text fil

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const path = require("path")
 const app = express()
 const routes = require('./routes/defaultroutes');
 
+
 app.set("views", path.join(__dirname, "views"))
 app.set("view engine", 'ejs');
 // Middleware to handle form data and JSON payloads
 
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // To handle URL-encoded data
